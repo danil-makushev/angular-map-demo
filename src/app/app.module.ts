84d24ed7f85689836d0c9f86d43212d1bbe22d84
Danil Makushev <reurairin@gmail.com>
2021-07-03 19:50:37 +0300

    add ui module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { ViewsModule } from './views/views.module';
+import { UiModule } from './ui/ui.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, LeafletModule, ViewsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LeafletModule,
+    ViewsModule,
+    UiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
